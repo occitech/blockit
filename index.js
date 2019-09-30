362920102d3c97aa259b3fae8722a8013ecb2c7f
Pierre Martin <pierre@occitech.fr>
2019-09-30 13:50:35 +0200

    Allow to customize the host, defaulting to 0.0.0.0

--- index.js
+++ index.js
@@ -60,8 +60,9 @@ app.get("/", (_, res) => {
   res.end("Hello world!");
 });
 
+const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 3000;
-app.listen(port, err => {
+app.listen(port, host, err => {
   if (err) throw err;
-  console.log(`> Running on localhost:${port}`);
+  console.log(`> Running on ${host}:${port}`);
 });
